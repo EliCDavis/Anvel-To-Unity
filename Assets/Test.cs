eb7efb135899e04cebf3d0acf0c976719be481a1
Eli C Davis <eli.davis1995@gmail.com>
2018-08-19 20:22:20 -0500

    Using multithreading to render lidar quickly. Getting updates from API in realtime

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -1,9 +1,14 @@
-﻿using System.Collections;
+﻿using Thrift.Protocol;
+using Thrift.Transport;
+using AnvelApi;
+
 using System.Collections.Generic;
 using UnityEngine;
 
 using CAVS.Anvel.Lidar;
 using CAVS.Anvel.Vehicle;
+using System.Threading;
+
 
 namespace CAVS
 {
@@ -11,12 +16,16 @@ namespace CAVS
     public class Test : MonoBehaviour
     {
 
-		private ParticleSystem particleSystem;
+        [SerializeField]
+        private string lidarSensorName;
+
+        private ParticleSystem particleSystem;
 
 
-		private List<TimeEntry> vehicleData;
+        private List<TimeEntry> vehicleData;
 
-		private List<GameObject> vehicleRender;
+
+        private List<GameObject> vehicleRender;
 
         DataPoint[] points;
 
@@ -28,17 +37,30 @@ namespace CAVS
 
         float timeSlice;
 
+        private AnvelControlService.Client anvelConnection;
+
+        private ObjectDescriptor lidarSensor;
+
+        private LidarPoints lidarPoints;
 
         // Use this for initialization
         void Start()
         {
-			particleSystem = gameObject.GetComponent<ParticleSystem>();
-			vehicleRender = new List<GameObject>();
-			
+            particles = new ParticleSystem.Particle[0];
+            lidarPoints = null;
+            anvelConnection = ConnectToAnvel();
+            lidarSensor = anvelConnection.GetObjectDescriptorByName(lidarSensorName);
+
+
+            // ======================= Rest of the shit =======================
+
+            particleSystem = gameObject.GetComponent<ParticleSystem>();
+            vehicleRender = new List<GameObject>();
+
             var lidarData = LidarSerialization.Load("360 Lidar-11.pcrp");
             points = lidarData.GetDataPoints();
 
-			vehicleData = VehicleLoader.LoadVehicleData("vehicle1_pos_2.vprp");
+            vehicleData = VehicleLoader.LoadVehicleData("vehicle1_pos_2.vprp");
 
             startTime = float.MaxValue;
             endTime = float.MinValue;
@@ -47,73 +69,143 @@ namespace CAVS
             {
                 if (point.TimeStamp < startTime)
                 {
-					startTime = point.TimeStamp;
-					startSlice = point.TimeStamp;
-                } 
-				if(point.TimeStamp > endTime) {
-					endTime = point.TimeStamp;
-				}
+                    startTime = point.TimeStamp;
+                    startSlice = point.TimeStamp;
+                }
+                if (point.TimeStamp > endTime)
+                {
+                    endTime = point.TimeStamp;
+                }
             }
-			timeSlice = .2f;
+            timeSlice = .2f;
 
-			Render();
+            Render();
+            //StartCoroutine(PollLidarPoints());
+            new Thread(PollLidarPoints).Start();
         }
 
-		private void Render() {
-			RenderLidar();
-			RenderVehicle();
-		}
+        ParticleSystem.Particle[] particles;
 
-		public void NewStartTime(float time){
-			startSlice = ((endTime - startTime) * time) + startTime;
-			Render();
-		}
+        private void PollLidarPoints (){
 
-		private void RenderLidar(){
-			List<ParticleSystem.Particle> particles = new List<ParticleSystem.Particle>();
-			foreach (var point in points)
+            while (true) {
+                var lidarPoints = anvelConnection.GetLidarPoints(lidarSensor.ObjectKey, 0);
+
+                var newParticles = new ParticleSystem.Particle[lidarPoints.Points.Count];
+
+                for (int i = 0; i < lidarPoints.Points.Count; i++)
+                {
+                    newParticles[i] = new ParticleSystem.Particle
+                    {
+                        remainingLifetime = float.MaxValue,
+                        position = new Vector3(
+                            (float)lidarPoints.Points[i].Y,
+                            (float)lidarPoints.Points[i].Z,
+                            (float)lidarPoints.Points[i].X),
+                        startSize = 1f,
+                        startColor = Color.white
+                    };
+                }
+                particles = newParticles;
+               // yield return null;
+            }
+        }
+
+        private void Update()
+        {
+            var toRender = particles;
+            if (toRender.Length > 0)
             {
-				if(point.TimeStamp > startSlice && point.TimeStamp < startSlice + timeSlice) {
-					var p = new ParticleSystem.Particle();
-					p.remainingLifetime = float.MaxValue;
-					p.position = point.Point;
-					p.startSize = 1f;
-					p.startColor = Color.white;
-					particles.Add(p);
-				}
-			}
-			particleSystem.Clear();
-			particleSystem.SetParticles(particles.ToArray(), particles.Count);
-		}
+                //particleSystem.Clear();
+                particleSystem.SetParticles(toRender, toRender.Length);
+            }
+        }
 
-		private void RenderVehicle() {
-			
-			// Clear current render data
-			foreach(var render in vehicleRender) {
-				Destroy(render);
-			}
-			vehicleRender.Clear();
 
-			// Find Entry to Render
-			TimeEntry closestEntry = null;
-			float closestDistance = float.MaxValue;
-			foreach(var entry in vehicleData) {
-				float distance = Mathf.Abs(startSlice - entry.Time);
-				if(distance < closestDistance) {
-					closestDistance = distance;
-					closestEntry = entry;
-				}
-			}
+        public static AnvelControlService.Client ConnectToAnvel(string ipAddress = "127.0.0.1", int port = 9094)
+        {
+            var transport = new TSocket(ipAddress, port);
+            var protocol = new TBinaryProtocol(transport);
+            var client = new AnvelControlService.Client(protocol);
 
-			// Render Entry
-			foreach(var trans in closestEntry.Transforms) {
-				var obj = GameObject.CreatePrimitive(PrimitiveType.Cube);
-				obj.transform.position = trans.Position;
-				obj.transform.rotation = trans.Rotation;
-				vehicleRender.Add(obj);
-			}
+            try
+            {
+                transport.Open();
+                transport.TcpClient.NoDelay = true;
+                return client;
+            }
+            catch (TTransportException e)
+            {
+                Debug.Log(string.Format("Error: {e.Message}"));
+                return null;
+            }
+        }
 
-		}
+        private void Render()
+        {
+            RenderLidar();
+            RenderVehicle();
+        }
+
+        public void NewStartTime(float time)
+        {
+            startSlice = ((endTime - startTime) * time) + startTime;
+            Render();
+        }
+
+        private void RenderLidar()
+        {
+            List<ParticleSystem.Particle> particles = new List<ParticleSystem.Particle>();
+            foreach (var point in points)
+            {
+                if (point.TimeStamp > startSlice && point.TimeStamp < startSlice + timeSlice)
+                {
+                    var p = new ParticleSystem.Particle
+                    {
+                        remainingLifetime = float.MaxValue,
+                        position = point.Point,
+                        startSize = 1f,
+                        startColor = Color.white
+                    };
+                    particles.Add(p);
+                }
+            }
+            particleSystem.Clear();
+            particleSystem.SetParticles(particles.ToArray(), particles.Count);
+        }
+
+        private void RenderVehicle()
+        {
+            // Clear current render data
+            foreach (var render in vehicleRender)
+            {
+                Destroy(render);
+            }
+            vehicleRender.Clear();
+
+            // Find Entry to Render
+            TimeEntry closestEntry = null;
+            float closestDistance = float.MaxValue;
+            foreach (var entry in vehicleData)
+            {
+                float distance = Mathf.Abs(startSlice - entry.Time);
+                if (distance < closestDistance)
+                {
+                    closestDistance = distance;
+                    closestEntry = entry;
+                }
+            }
+
+            // Render Entry
+            foreach (var trans in closestEntry.Transforms)
+            {
+                var obj = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                obj.transform.position = trans.Position;
+                obj.transform.rotation = trans.Rotation;
+                vehicleRender.Add(obj);
+            }
+
+        }
 
     }
 
--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 Code to interface with Anvel data inside of Unity.
 
+Requires Unity 2018's .NET 4.x profile to properly use Anvel's dlls.
+
 ## Lidar and Vehicle Data
 
 Supports lidar playback files (.pcrp) and vehicle playback files (vprp)
